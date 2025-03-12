2f9ca2578a207971aec2c573a998d09d17ebd2c2
C3ald <specklesjr10@gmail.com>
2025-03-12 15:19:45 -0700

    clean up in main.go

--- main.go
+++ main.go
@@ -1,32 +1,12 @@
 package main
 
 import (
-	// "fmt"
-	// "katz/katz/modules"
-	// "katz/katz/style"
 
-	// "katz/katz/utils"
-	// // "log"
-	// // "strings"
-	// // "fmt"
-
-	// // "encoding/base64"
-	// "fmt"
-	// test "katz/katz/modules/kerb"
-	// "os"
 	cli "katz/katz/cli"
 )
 
 
 func main() {
 
-		// systoken, _ := utils.GetSystem()
-		// defer systoken.Close()
-		// utils.InjectToken(systoken)
-
-		// ticketData, _ := test.TGT("test.local", "Administrator", "password")
-		// fmt.Println(ticketData.Ticket.EncodedTicketSize)
-
-	// }
 	cli.Init()
 }
